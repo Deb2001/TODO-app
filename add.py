e2bf956f4764f14d5f985634a35e56509ecc77c6
Rohan <rohan.bhattacharjee.77@gmail.com>
2021-08-23 14:57:41 +0530

    Improved same date tasks

--- add.py
+++ add.py
@@ -9,19 +9,23 @@ class Add_data():
                 "description":desc,
                 }]
             }
-        if date in new_data:
-            new_data[date].append({"title":title,"description":desc})    
+        
         try:
             with open("data.json", "r") as file:
                 #Reading old data
                 data = json.load(file)
+                
         except FileNotFoundError:
             with open("data.json", "w") as file:
                 json.dump(new_data, file, indent=4)
         else:
-            #Updating old data with new data
-            data.update(new_data)
-
+            if date in data:
+                data[date].append({"title":title,"description":desc})    
+                #updating data by appending same date tasks
+                data.update(data)
+            else:
+                #normally updating tasks of different dates
+                data.update(new_data)    
             with open("data.json", "w") as file:
                 #Saving updated data
                 json.dump(data, file, indent=4)
