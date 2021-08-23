3b355c344a37132eb77cfa2edb90d84adbfaa7a2
Debjyoti Ghosh <debjyotighosh06000@gmail.com>
2021-08-23 20:03:43 +0530

    made changes to add and search functionality

--- search.py
+++ search.py
@@ -2,25 +2,28 @@ from tkinter import *
 from tkinter import messagebox
 import json
 class Find():
-    def __init__(self):
+    def init(self):
         pass
     def search(self,date):
-        
+
         try:
             with open("data.json") as data_file:
                 data=json.load(data_file)
-            
+
         except FileNotFoundError:
             messagebox.showinfo(title="Error", message="No Data File Found.")
-        
+
         else:
-            if date in data:
-                title=data[date]["title"]
-                description=data[date]["description"]
-                messagebox.showinfo(title=date, message=f"Tile: {title}\nDescription: {description}")
-                
+            if date in data :
+                str=""
+                c=1
+                for dict in data[date]:
+                    title=dict["title"]
+                    description=dict["description"]
+                    str=f" {str}\n "+ f" \nTask - {c}"+f" \nTitle : {title} "+f" \nDescription: {description} \n"
+                    c=c+1
+                #messagebox.showinfo(title=date, message=f"Title: {title}\nDescription: {description}")
+                messagebox.showinfo(title=date, message=f"List of Tasks\n {str}")
 
             else:
                 messagebox.showinfo(title="Error",message="No such todo exist")
-        
-
